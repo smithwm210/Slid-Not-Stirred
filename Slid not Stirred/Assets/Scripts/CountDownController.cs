33b16d9cd6906d7eb5fe14f7206af17c111add4a
smithwm210 <smithwm210@gmail.com>
2023-04-10 15:24:42 -0400

    countdown text (feel free to change)

--- Slid not Stirred/Assets/Scripts/CountDownController.cs
+++ Slid not Stirred/Assets/Scripts/CountDownController.cs
@@ -32,7 +32,7 @@ public class CountDownController : MonoBehaviour
                 timerDis.text = "Go!";
             }
 
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(0.75f);
 
             countDownTimer--;
 
