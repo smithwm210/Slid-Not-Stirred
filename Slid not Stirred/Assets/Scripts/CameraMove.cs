fcd897e956319819c53bffae316ec05897a84920
smithwm210 <smithwm210@gmail.com>
2023-04-10 17:51:14 -0400

    added camera upper bound

--- Slid not Stirred/Assets/Scripts/CameraMove.cs
+++ Slid not Stirred/Assets/Scripts/CameraMove.cs
@@ -14,7 +14,7 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x + xOffset ,target.position.y+ yOffset,-10f);
+        Vector3 newPos = new Vector3(target.position.x + xOffset, Mathf.Min((target.position.y + yOffset), 5.4f), -10f);
         transform.position = Vector3.Slerp(transform.position,newPos,followSpeed*Time.deltaTime);
     }
 }
